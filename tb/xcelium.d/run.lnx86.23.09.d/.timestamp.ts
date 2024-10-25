58959b90b97d236ec64f7f94ae5c6e23cbad311e
nguyendaithien <nluu1784@gmail.com>
2024-10-25 10:17:14 +0700

    final version

--- tb/xcelium.d/run.lnx86.23.09.d/.timestamp.ts
+++
@@ -1,4 +0,0 @@
-1728100787 /home/usr3/project/CPU_5stage_pipeline/tb/pkg_1.sv
-1728100791 /home/usr3/project/CPU_5stage_pipeline/tb/check_FSM.sv
-1728226616 /home/usr3/project/CPU_5stage_pipeline/tb/pkg_rd_gen.sv
-1728226780 /home/usr3/project/CPU_5stage_pipeline/tb/random_gen.sv
--- tb/xcelium.d/run.lnx86.23.09.d/files.ts
+++
@@ -1,4 +0,0 @@
-1728226780 /home/usr3/project/CPU_5stage_pipeline/tb/random_gen.sv
-1728136026 /home/usr3/project/CPU_5stage_pipeline/tb/check_FSM.sv
-1728226010 /home/usr3/project/CPU_5stage_pipeline/tb/pkg_1.sv
-1728226616 /home/usr3/project/CPU_5stage_pipeline/tb/pkg_rd_gen.sv
